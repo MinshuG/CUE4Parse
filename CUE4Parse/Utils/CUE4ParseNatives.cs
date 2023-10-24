f12e3afe62db9672c78df3c4da123b9a0366d0f1
MountainFlash <65584814+MinshuG@users.noreply.github.com>
2023-10-24 18:29:48 +0530

    Oodle Natives

---
+++ CUE4Parse/Utils/CUE4ParseNatives.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace CUE4Parse.Utils; 
+
+public static class CUE4ParseNatives 
+{
+    [DllImport("CUE4Parse-Natives", CallingConvention = CallingConvention.Cdecl)]
+    public static extern bool IsFeatureAvailable([MarshalAs(UnmanagedType.LPStr)] string featureName); 
+}
